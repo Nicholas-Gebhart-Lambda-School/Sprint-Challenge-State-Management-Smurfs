a03d0ed622577f1768ac59682f6afe71512e1d37
gebhartn <nicholas.gebhart@gmail.com>
2019-10-11 08:23:45 -0700

    add fetch

--- smurfs/src/actions/action.creators.js
+++ smurfs/src/actions/action.creators.js
@@ -1,2 +1,17 @@
 import axios from 'axios';
 import { FETCH_PENDING, FETCH_SUCCESS, FETCH_FAILURE } from './action.types';
+
+export const fetchSmurf = () => dispatch => {
+  dispatch({ type: FETCH_PENDING });
+
+  axios
+    .get('localhost:3333/smurfs')
+    .then(res => {
+      console.log(res.data);
+      dispatch({ type: FETCH_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log('error in fetch', err);
+      dispatch({ type: FETCH_FAILURE, payload: err.response });
+    });
+};
--- smurfs/src/reducers/smurf.reducer.js
+++ smurfs/src/reducers/smurf.reducer.js
@@ -1,3 +1,9 @@
+import {
+  FETCH_PENDING,
+  FETCH_SUCCESS,
+  FETCH_FAILURE
+} from '../actions/action.types';
+
 const initialState = {
   fetching: false,
   fetched: false,
@@ -7,6 +13,27 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_PENDING:
+      return {
+        ...state,
+        fetching: true,
+        error: ''
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        smurfs: [...state.smurfs, action.payload],
+        error: ''
+      };
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        fetched: false,
+        error: action.payload
+      };
     default:
       return state;
   }
